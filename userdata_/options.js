a605296d630b7115f08ee93dcaaf858b0ef755ab
dagalufh <dagalufh+git@gmail.com>
2015-04-29 20:17:34 +0200

    Corrected an issue for first time run

---
+++ userdata_/options.js
@@ -0,0 +1,9 @@
+{
+  "city": "skovde",
+  "port": "8888",
+  "doubletapcount": "3",
+  "doubletapseconds": "5",
+  "autoremote_key": "A key",
+  "autoremote_password": "",
+  "autoremote_message": "Device {device-id} was set to {device-lastcommand}"
+}
