b72db5c4a827c86bdaa44ffb69098a4c007642e3
dagalufh <dagalufh+git@gmail.com>
2015-05-03 20:50:32 +0200

    First attempt at a WindowsInstaller.
    The WindowsInstaller.js is to be run by 'node WindowsInstaller.js'

---
+++ WindowsInstaller.js
@@ -0,0 +1,47 @@
+console.log('This will try to install the application for you.');
+console.log('This includes installing node-windows if not already installed. Linking directories for this and running npm install to install dependencies.');
+console.log('The end result is a service running with this program.');
+
+var exec = require('child_process').exec;
+console.log('Checking to see if node-windows is installed');
+exec('npm list -g node-windows', null, function (error,stdout,stderr) {
+    
+    if (stdout.toString().indexOf('empty') !== -1) {
+        console.log('node-windows is not installed globally.');
+        
+        // Install node-windows if it's not installed.
+        exec('npm install -g node-windows', null, function (error,stdout,stderr) {
+            if (stdout.toString().indexOf('node-windows')) {
+                
+                exec('npm link node-windows', null, function (error,stdout,stderr) {
+                    console.log('Linked node-windows with current directory.');
+                    console.log('Trying to installing required modules');
+                    exec('npm install', null, function (error,stdout,stderr) {
+                        console.log('Installed the dependencies for the application');
+                        console.log('This will need elevated permissions but that will be asked for when needed.');
+                        exec('npm service_install.js', null, function (error,stdout,stderr) {
+                            console.log('Installed the service');
+                            console.log('The service should be started automatically. If there is an error, check in the daemon folder.');
+                            console.log('Check if you can reach the application on the predefined address: http://127.0.0.1:8888');
+                        });
+                    });
+                });
+            }
+        });
+    } else {
+        console.log('node-windows is already installed. No need to install it twice.');
+        exec('npm link node-windows', null, function (error,stdout,stderr) {
+            console.log('Trying to installing required modules');
+            exec('npm install', null, function (error,stdout,stderr) {
+                console.log('Installed the dependencies for the application');
+                console.log('This will need elevated permissions but that will be asked for when needed.');
+                exec('npm service_install.js', null, function (error,stdout,stderr) {
+                    console.log('Installed the service');
+                    console.log('The service should be started automatically. If there is an error, check in the daemon folder.');
+                    console.log('Check if you can reach the application on the predefined address: http://127.0.0.1:8888');
+                });
+            });
+           
+        });
+    }
+});
