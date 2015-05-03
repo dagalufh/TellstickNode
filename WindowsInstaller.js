f8106b5ed4ddcb573501020ec16c58eea079e527
dagalufh <dagalufh+git@gmail.com>
2015-05-03 21:06:14 +0200

    Corrected issues with WindowsInstaller
    Semantic errors.
    Tested on a Windows 7, 32-bit.
    Pre-requisite: NodeJS installed. Thats it.

--- WindowsInstaller.js
+++ WindowsInstaller.js
@@ -11,33 +11,36 @@ exec('npm list -g node-windows', null, function (error,stdout,stderr) {
         
         // Install node-windows if it's not installed.
         exec('npm install -g node-windows', null, function (error,stdout,stderr) {
-            if (stdout.toString().indexOf('node-windows')) {
-                
                 exec('npm link node-windows', null, function (error,stdout,stderr) {
+                    console.log(stdout);
                     console.log('Linked node-windows with current directory.');
                     console.log('Trying to installing required modules');
                     exec('npm install', null, function (error,stdout,stderr) {
+                        console.log(stdout);
                         console.log('Installed the dependencies for the application');
                         console.log('This will need elevated permissions but that will be asked for when needed.');
-                        exec('npm service_install.js', null, function (error,stdout,stderr) {
+                        exec('node service_install.js', null, function (error,stdout,stderr) {
+                            console.log(stdout);
                             console.log('Installed the service');
-                            console.log('The service should be started automatically. If there is an error, check in the daemon folder.');
+                            console.log('The service should be started automatically. Otherwise you can start it by going into services and start the "TellstickNode" service. If there is an error, check in the daemon folder.');
                             console.log('Check if you can reach the application on the predefined address: http://127.0.0.1:8888');
                         });
                     });
                 });
-            }
         });
     } else {
         console.log('node-windows is already installed. No need to install it twice.');
         exec('npm link node-windows', null, function (error,stdout,stderr) {
+            console.log(stdout);
             console.log('Trying to installing required modules');
             exec('npm install', null, function (error,stdout,stderr) {
+                console.log(stdout);
                 console.log('Installed the dependencies for the application');
                 console.log('This will need elevated permissions but that will be asked for when needed.');
-                exec('npm service_install.js', null, function (error,stdout,stderr) {
+                exec('node service_install.js', null, function (error,stdout,stderr) {
+                    console.log(stdout);
                     console.log('Installed the service');
-                    console.log('The service should be started automatically. If there is an error, check in the daemon folder.');
+                    console.log('The service should be started automatically. Otherwise you can start it by going into services and start the "TellstickNode" service. If there is an error, check in the daemon folder.');
                     console.log('Check if you can reach the application on the predefined address: http://127.0.0.1:8888');
                 });
             });
